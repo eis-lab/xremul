768cc0a2e2be7fb4b0019bec1f5b556dd5370d37
Jaewon Choi <jaewon.james.choi@gmail.com>
2019-02-20 15:20:51 +0900

    Sphere blinks by red!

--- XREmul/Assets/Scripts/AppManager.cs
+++ XREmul/Assets/Scripts/AppManager.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AppManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Renderer _lastColliderRenderer;
+    public Camera activeCamera;
+
+    private void Start()
     {
-        
+        // Look at sphere position
+        activeCamera.transform.LookAt(Vector3.zero);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var ray = activeCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo))
+        {
+            var colliderGameObject = hitInfo.collider.gameObject;
+
+            var colliderRenderer = colliderGameObject.GetComponent<Renderer>();
+
+            colliderRenderer.material.color = Color.red;
+
+            _lastColliderRenderer = colliderRenderer;
+        }
+        else
+        {
+            if (_lastColliderRenderer)
+                _lastColliderRenderer.material.color = Color.white;
+        }
     }
 }
