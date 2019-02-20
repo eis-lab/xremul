a59c4836d5771bb913175a40623b3927a569f039
Jaewon Choi <jaewon.james.choi@gmail.com>
2019-02-20 18:44:14 +0900

    Add record-and-replay functionality

--- XREmul/Assets/Scripts/AppManager.cs
+++ XREmul/Assets/Scripts/AppManager.cs
@@ -1,5 +1,8 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AppManager : MonoBehaviour
 {
@@ -9,19 +12,44 @@ public class AppManager : MonoBehaviour
         CameraDirection
     }
 
-    private Renderer _lastColliderRenderer;
-    public Camera activeCamera;
+    private readonly List<CameraTransformLog> _cameraTransformLogs = new List<CameraTransformLog>();
 
-    public CollidingState currentCollidingState = CollidingState.ManualMouse;
+    private Camera _activeCamera;
+
+    private Renderer _lastColliderRenderer;
+
+    private double _recordingTimer;
+    private double _replayingTimer;
+
+    public CollidingState currentCollidingState = CollidingState.CameraDirection;
+
+    [FormerlySerializedAs("activeCamera")] public Camera recordingCamera;
+    public Camera replayingCamera;
+
+    public bool IsRecording { get; set; }
+    public bool IsReplaying { get; set; }
 
     private void Start()
     {
         // Look at sphere position
-        activeCamera.transform.LookAt(Vector3.zero);
+        recordingCamera.transform.LookAt(Vector3.zero);
     }
 
     private void Update()
     {
+        OnCollideWithSphereOrNot();
+
+        if (IsRecording)
+            OnRecord();
+        if (IsReplaying)
+            OnReplay();
+    }
+
+    private void OnCollideWithSphereOrNot()
+    {
+        if (!_activeCamera)
+            return;
+
         GetRayFrom getRayFromFunc;
 
         switch (currentCollidingState)
@@ -41,7 +69,7 @@ public class AppManager : MonoBehaviour
                 throw new Exception("You're not written all of the enum config");
         }
 
-        var ray = getRayFromFunc(activeCamera);
+        var ray = getRayFromFunc(_activeCamera);
 
         if (Physics.Raycast(ray, out var hitInfo))
         {
@@ -49,7 +77,7 @@ public class AppManager : MonoBehaviour
 
             var colliderRenderer = colliderGameObject.GetComponent<Renderer>();
 
-            colliderRenderer.material.color = Color.red;
+            colliderRenderer.material.color = Color.red; // 아예 이런 종류의 Renderer를 굽는 방법도 있음.
 
             _lastColliderRenderer = colliderRenderer;
         }
@@ -59,4 +87,80 @@ public class AppManager : MonoBehaviour
                 _lastColliderRenderer.material.color = Color.white;
         }
     }
+
+    public void OnRecord()
+    {
+        double kFixedRecordingTime = 10;
+
+        var cameraTransform = recordingCamera.transform;
+
+        _cameraTransformLogs.Add(new CameraTransformLog
+        {
+            timestamp = _recordingTimer,
+            position = cameraTransform.position,
+            rotation = cameraTransform.rotation
+        });
+
+        if (_recordingTimer > kFixedRecordingTime)
+        {
+            ResetRecordingTimer();
+            IsRecording = false;
+
+            print("Recording Is Over");
+        }
+
+        _recordingTimer += Time.deltaTime;
+    }
+
+    public void OnReplay()
+    {
+        var kThreshold = 0.1;
+
+        var matchedLog = _cameraTransformLogs
+            .FirstOrDefault(log => log.timestamp > _replayingTimer);
+
+        if (matchedLog != null)
+        {
+            replayingCamera.transform.position = matchedLog.position;
+            replayingCamera.transform.rotation = matchedLog.rotation;
+        }
+        else
+        {
+            ResetReplayingTimer();
+            IsReplaying = false;
+
+            print("Replaying Is Over");
+        }
+
+        _replayingTimer += Time.deltaTime;
+    }
+
+    public void ResetRecordingTimer()
+    {
+        recordingCamera.enabled = true;
+        replayingCamera.enabled = false;
+
+        _activeCamera = recordingCamera;
+
+        _recordingTimer = 0.0f;
+    }
+
+    public void ResetReplayingTimer() // FIXME: Generalize for the file based log. This is only for memory based log.
+    {
+        recordingCamera.enabled = false;
+        replayingCamera.enabled = true;
+
+        _activeCamera = replayingCamera;
+
+        _replayingTimer = 0.0f;
+    }
+
+    private class CameraTransformLog
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public double timestamp;
+    }
+
+    private delegate Ray GetRayFrom(Camera camera);
 }
---
+++ XREmul/Assets/Scripts/AppManagerEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AppManager))]
+public class AppManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var appManager = (AppManager) target;
+
+        if (GUILayout.Button("Record"))
+        {
+            appManager.ResetRecordingTimer();
+            appManager.IsRecording = true;
+        }
+
+        if (GUILayout.Button("Replay"))
+        {
+            appManager.ResetReplayingTimer();
+            appManager.IsReplaying = true;
+        }
+
+        if (GUILayout.Button("Stop"))
+        {
+            appManager.IsRecording = false;
+            appManager.IsReplaying = false;
+        }
+    }
+}
