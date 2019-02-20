2dc735deb01838cabbe09d56aed381f53cd204ef
Jaewon Choi <jaewon.james.choi@gmail.com>
2019-02-20 16:02:52 +0900

    Make blinking input options

--- XREmul/Assets/Scripts/AppManager.cs
+++ XREmul/Assets/Scripts/AppManager.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+using System;
+using UnityEngine;
 
 public class AppManager : MonoBehaviour
 {
+    public enum CollidingState
+    {
+        ManualMouse,
+        CameraDirection
+    }
+
     private Renderer _lastColliderRenderer;
     public Camera activeCamera;
+
+    public CollidingState currentCollidingState = CollidingState.ManualMouse;
 
     private void Start()
     {
@@ -13,7 +22,26 @@ public class AppManager : MonoBehaviour
 
     private void Update()
     {
-        var ray = activeCamera.ScreenPointToRay(Input.mousePosition);
+        GetRayFrom getRayFromFunc;
+
+        switch (currentCollidingState)
+        {
+            case CollidingState.ManualMouse:
+                getRayFromFunc = cam => cam.ScreenPointToRay(Input.mousePosition);
+                break;
+            case CollidingState.CameraDirection:
+                getRayFromFunc = cam =>
+                {
+                    var transform = cam.transform;
+
+                    return new Ray(transform.position, transform.forward);
+                };
+                break;
+            default:
+                throw new Exception("You're not written all of the enum config");
+        }
+
+        var ray = getRayFromFunc(activeCamera);
 
         if (Physics.Raycast(ray, out var hitInfo))
         {
